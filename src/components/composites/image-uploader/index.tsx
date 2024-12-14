1407ce9cdfd0fb50e1be27f01316b1a57c144cab
nasustim <mitsuhibino@gmail.com>
2024-12-14 16:39:49 +0900

    feat: load image feature

---
+++ src/components/composites/image-uploader/index.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { FC } from "react"
+
+type Props = {
+  onUpload: (file: File) => void
+}
+
+export const ImageUploader: FC<Props> = ({ onUpload }) => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (!file) return // todo: handle error
+
+    onUpload(file)
+  }
+
+  return <input type='file' accept='image/*' onChange={onChange} />
+}
